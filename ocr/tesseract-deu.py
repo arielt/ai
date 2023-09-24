d7d8151cd71ed3ba854f5d7e5b5115fae41952c2
Ariel Tubaltsev <ariel@zxbase.com>
2023-09-24 16:09:04 -0700

    OCR: Tesseract: german: adjust cv2
    show window together with extracted text
    wait for user to press button in cv2
    close all cv2 windows

--- ocr/tesseract-deu.py
+++ ocr/tesseract-deu.py
@@ -3,7 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # read the image
-img = cv2.imread('ocr/img/tesseract-sample.png', 0)
+img = cv2.imread('ocr/img/receipt-deu.png', 0)
 # cv2.imshow('Original image', img)
 # wait 3 seconds
 # key = cv2.waitKey(3000)
@@ -26,9 +26,14 @@ for i in range(n_boxes):
     (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])    
     img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
+cv2.namedWindow('Tesseract')
+cv2.moveWindow('Tesseract', 400, 0)
 cv2.imshow('Tesseract', img)
-key = cv2.waitKey(1000)
 
 extracted_text = pytesseract.image_to_string(img, lang = 'deu')
 print('Extracted text', extracted_text)
-input('Press Enter to continue...')
+
+# waits for user to press any key
+cv2.waitKey(0)
+# closing all open windows
+cv2.destroyAllWindows()

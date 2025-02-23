1f8e5311e08ffac1a7a0f09ac05e043029fd72fd
Ariel Tubaltsev <tubaltzev@gmail.com>
2025-02-23 12:33:36 -0800

    sentiment: TextBlob calculation: add multiple columns at once

--- sentiment/calculate_text_blob.py
+++ sentiment/calculate_text_blob.py
@@ -6,9 +6,13 @@ from tqdm import tqdm
 
 from textblob import TextBlob
 
-def calculate_sentiment(text):
+def calculate_sentiment(s):
     """Sentiment calculation."""
-    return TextBlob(str(text)).sentiment
+    tb = TextBlob(str(s.Quote))
+    s['Sentiment'] = tb.sentiment
+    s['Polarity'] = tb.sentiment.polarity
+    s['Subjectivity'] = tb.sentiment.subjectivity
+    return s
 
 path = kagglehub.dataset_download("manann/quotes-500k")
 
@@ -17,7 +21,10 @@ df = pd.read_csv(path + '/quotes.csv')
 df.columns = ['Quote', 'Author', 'Tags']
 
 tqdm.pandas()
-df['Sentiment'] = df['Quote'].progress_apply(calculate_sentiment)
+df_out = df.progress_apply(calculate_sentiment, axis=1)
+
+# for testing, use subset
+# df_out = df.iloc[:5].progress_apply(calculate_sentiment, axis=1)
 
 # save results
-df.to_csv('./data/text-blob-sentiment.csv')
+df_out.to_csv('./data/text-blob-sentiment.csv')

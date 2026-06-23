7f9b62153e997684a52eb5cb5e1133e996c796cf
Ariel Tubaltsev <tubaltzev@gmail.com>
2026-06-23 09:36:38 -0700

    llm: organize using transformers chapter

--- llm/using_transformers/multiple_sequences.py
+++ llm/using_transformers/multiple_sequences.py
@@ -49,3 +49,16 @@ attention_mask = [
 
 outputs = model(torch.tensor(batched_ids), attention_mask=torch.tensor(attention_mask))
 print(outputs.logits)
+
+## From tokenizer to model
+print("\n --- From tokernizer to model")
+import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+checkpoint = "distilbert-base-uncased-finetuned-sst-2-english"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+model = AutoModelForSequenceClassification.from_pretrained(checkpoint)
+sequences = ["I've been waiting for a HuggingFace course my whole life.", "So have I!"]
+
+tokens = tokenizer(sequences, padding=True, truncation=True, return_tensors="pt")
+output = model(**tokens)
